5ce9d4f04dcc295a597d461eb8b8ed5510208f86
Felipe Chaulet <felipechaulet@gmail.com>
2023-11-29 23:02:18 +0100

    added result to log

--- src/main.js
+++ src/main.js
@@ -120,6 +120,7 @@ const runPuppeteer = async (url) => {
 
     console.log('parsing funda.nl data');
     const result = dom.window.document.querySelectorAll('.search_result');
+    console.log(result);
     for (const element of result) {
         const urlPath = element?.querySelectorAll('a')?.[0]?.href;
         const headerSubtitle = element?.querySelector('.search_result__header-subtitle');
