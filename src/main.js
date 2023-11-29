299fb0a1334c6b886e07b59af4eeb13b5517a9dc
Felipe Chaulet <felipechaulet@gmail.com>
2023-11-29 23:26:57 +0100

    changed div

--- src/main.js
+++ src/main.js
@@ -119,7 +119,7 @@ const runPuppeteer = async (url) => {
 
 
     console.log('parsing funda.nl data');
-    const result = dom.window.document.querySelectorAll('div.pb-4');
+    const result = dom.window.document.querySelectorAll('div.sm:flex');
     console.log(result.length);
     for (const element of result) {
         const urlPath = element?.querySelectorAll('a')?.[0]?.href;
